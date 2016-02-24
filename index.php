ecfbea1d7abdb1069c510b5fb4e262a1211d98c4
CD-CMGM-X <cgomez@consultoriadual.com>
2016-02-24 12:38:38 -0500

    Add index.php

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php 
+//We open a session to work all the proyect at the same language.
+session_start();
+include 'vistas/header.php';
+include 'vistas/content.php';
+include 'vistas/footer.php';
+//include 'negocio/gettext.neg.php';
+?>
